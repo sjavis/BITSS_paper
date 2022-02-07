e93a3537f65d1f3f46a86c54a0523c903fd92fb8
Sam Avis <avissam96@gmail.com>
2022-02-07 22:20:12 +0000

    SI: Constraint coefficients sections done.

---
+++ si/fig/paramtest/plot.py
@@ -0,0 +1,53 @@
+import copy
+import json
+import numpy as np
+from matplotlib import pyplot as plt, cm
+from scipy.ndimage import gaussian_filter
+from pyutils import latexify as lt
+# Get default style for SI
+import sys; sys.path.insert(0, '..')
+import style
+
+subfigs = ['a', 'b']
+systems = ['lj', 'cb']
+levels = [[600, 1000, 2000],
+          [42000, 45000, 50000]]
+label_locs = [[(4.5, 0.1), (10, 0.1), (50,0.19)],
+              [(10, 0.03), (18, 0.13), (180, 0.05)]]
+
+cmap = copy.copy(cm.get_cmap('viridis'))
+cmap.set_bad('lightgrey')
+
+with open('data.json', 'r') as f:
+    data = json.load(f)
+for i, system in enumerate(systems):
+    d = data[system]
+    shape = (len(np.unique(d['alpha'])), -1)
+    alpha = np.reshape(d['alpha'], shape)
+    beta = np.reshape(d['beta'], shape)
+    fc = np.reshape(d['fc'], shape)
+    
+    fig = lt.figure(0.5)
+    fig.text(0, 1, f'({subfigs[i]})', ha='left', va='top')
+    ax = fig.add_axes((0.15, 0.17, 0.78, 0.81))
+    plt.semilogx()
+    plt.semilogy()
+
+    # Colormap
+    plt.pcolormesh(alpha, beta, fc, shading='nearest', cmap=cmap, vmax=np.nanpercentile(fc,90), ec='face', lw=0.1)
+    plt.colorbar(label='function calls')
+
+    # Contours
+    filter1 = gaussian_filter(np.where(np.isnan(fc), 0., fc), 1, truncate=1)
+    filter2 = gaussian_filter(np.where(np.isnan(fc), 0., 1.), 1, truncate=1)
+    filtered = np.where(np.isnan(fc), np.nan, filter1/filter2)
+    contour = plt.contour(alpha, beta, filtered, colors='w', levels=levels[i])
+    plt.clabel(contour, fmt="%d", manual=label_locs[i])
+
+    plt.plot([10], [0.1], marker='*', c='r', zorder=99)
+
+    plt.xlabel(r'$\alpha$')
+    plt.ylabel(r'$\beta$')
+    plt.xticks([0.1, 1, 10, 100, 1000, 10000])
+    plt.minorticks_off()
+    lt.savefig(f'../paramtest-{subfigs[i]}.pdf')
---
+++ si/fig/style.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+import pyutils.latexify as lt
+
+lt.update_width('r4')
+plt.rc('font', size=9)
+plt.rc('axes', labelsize=9)
+
+c1 = 'tab:orange'
+c2 = 'tab:red'
+c3 = 'tab:blue'
+colors = [c1, c2, c3]
+
+
+### Make sure this is reloaded by IPython ###
+# exit_register runs at the end of ipython %run or the end of the python interpreter
+from IPython import get_ipython
+ip = get_ipython()
+if (ip == None):
+    from atexit import register as exit_register
+else:
+    def exit_register(fun, *args, **kwargs):
+        """ Decorator that registers at post_execute. After its execution it
+        unregisters itself for subsequent runs. """
+        def callback():
+            fun()
+            ip.events.unregister('post_execute', callback)
+        ip.events.register('post_execute', callback)
+@exit_register
+def reset_rcparams():
+    # Ensure IPython reloads the module
+    import sys
+    del sys.modules['style']
