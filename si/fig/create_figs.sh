2809c2cf2e501ca5ce3730d91b0729a4ddd8a7bd
Sam Avis <avissam96@gmail.com>
2022-02-10 16:21:00 +0000

    Added SI figures and bar and hinge gradients

---
+++ si/fig/create_figs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+py_figs=('paramtest')
+tex_figs=('BAHschematic')
+
+for fig in ${py_figs[@]}; do
+  echo "Plotting $fig"
+  cd make_$fig
+  python3 ./plot.py
+  cd ..
+done
+
+for fig in ${tex_figs[@]}; do
+  echo "Plotting $fig"
+  pdflatex -interaction=nonstopmode -file-line-error -jobname=$fig make_$fig/plot.tex | \
+    grep -v "[0-9]*/[0-9]*/[0-9]*/[0-9]*:[0-9]*:[0-9]*" | \
+    grep ".*:[0-9]*:\|Warning\|Overfull" --color=always
+  rm $fig.aux
+  rm $fig.log
+done
