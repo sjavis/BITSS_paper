cf61c3ed7c982c803ebffd6b91351ed37e2270e5
Sam Avis <avissam96@gmail.com>
2022-03-15 22:58:04 +0000

    Updated insert_bib script

--- insert_bib.sh
+++ insert_bib.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
 
-start_tag='% start bib'
-end_tag='% end bib'
+start_tag='%% start bib'
+mid_tag='%% mid bib'
+end_tag='%% end bib'
 
 BBLFILE="${1%.tex}.bbl"
+AUXFILE="${1%.tex}.aux"
 if [ ! -f "$1" ]; then
-  echo "File not found"
-  exit 1
-elif [ ! -f "$BBLFILE" ]; then
-  echo "BBL file does not exist. Run bibtex on file."
+  echo "Error: File not found."
   exit 1
 fi
 
-# Remove commented lines
+# Use bibtex to generate aux file
+sed -i "/$start_tag/,/$mid_tag/ {//b; /$start_tag/b; s/^%\s*//}" $1 # Uncomment
+sed -i "/$mid_tag/,/$end_tag/ {//b; /$mid_tag/b; /^%/b; s/^/% /}" $1 # Comment
+latex -interaction=batchmode "$1" > /dev/null 2>&1
+sed -i "/$start_tag/,/$mid_tag/ {//b; /$start_tag/b; s/^/% /}" $1 # Comment
+sed -i "/$mid_tag/,/$end_tag/ {//b; /$mid_tag/b; s/^%\s*//}" $1 # Uncomment
+if [ ! -f "$AUXFILE" ]; then
+  echo "Error: Failure generating aux file."
+  exit 1
+fi
+
+# Generate bbl file
+bibtex -terse "$AUXFILE"
+if [ ! -f "$BBLFILE" ]; then
+  echo "Error: Failure generating bbl file."
+  exit 1
+fi
 sed -i "/^%/d" $BBLFILE
 
-# Delete between two tags
-sed -i "/${start_tag}/,/${end_tag}/{//!d}" $1
-
-# Paste bibliography between two tags
-sed -i "/${start_tag}/r $BBLFILE" $1
+# Paste bibliography into tex file
+sed -i "/${mid_tag}/,/${end_tag}/{//!d}" $1
+sed -i "/${mid_tag}/r $BBLFILE" $1
