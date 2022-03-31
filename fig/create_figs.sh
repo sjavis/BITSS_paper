5cb1386b7ed29354e7d13646c7d278b5cb0262ed
Sam Avis <avissam96@gmail.com>
2022-03-31 12:45:25 +0100

    Moved example system details to methods

--- fig/create_figs.sh
+++ fig/create_figs.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-figures=('toy2d' 'speedtest' 'adaptivemesh' 'flatdiscontinuous')
+py_figs=('toy2d' 'speedtest' 'adaptivemesh' 'flatdiscontinuous')
+tex_figs=('BAHschematic')
 
-for fig in ${figures[@]}; do
+for fig in ${py_figs[@]}; do
   echo "Plotting $fig"
   cd make_$fig
   python3 ./plot.py
   cd ..
+done
+
+for fig in ${tex_figs[@]}; do
+  echo "Plotting $fig"
+  pdflatex -interaction=nonstopmode -file-line-error -jobname=$fig make_$fig/plot.tex | \
+    grep -v "[0-9]*/[0-9]*/[0-9]*/[0-9]*:[0-9]*:[0-9]*" | \
+    grep ".*:[0-9]*:\|Warning\|Overfull" --color=always
+  rm $fig.aux
+  rm $fig.log
 done
